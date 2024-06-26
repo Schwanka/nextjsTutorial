0c9f3a7a257cb9000a00d5ca4b31664c205d5600
Amora <amoraschwanka@gmail.com>
2024-06-26 11:29:45 -0300

    updating invoces so it can handle errors and not found id's on update.

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -146,6 +146,7 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
+  
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -156,13 +157,12 @@ export async function fetchInvoiceById(id: string) {
       FROM invoices
       WHERE invoices.id = ${id};
     `;
-
     const invoice = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
